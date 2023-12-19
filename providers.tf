2f032710b0cbefda3814784ca82dae03f9e4d6ec
shankardhandapani <shankardhandapani1@gmail.com>
2023-12-19 19:10:41 +0530

    updated backend conf

--- providers.tf
+++ providers.tf
@@ -19,8 +19,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "tfconfig"
-    storage_account_name = "shankarapollopoctfstate"
+    resource_group_name  = "shankar-rg"
+    storage_account_name = "apollopocshankar"
     container_name       = "tfstate"
     key                  = "akspoc.tfstate"
     subscription_id      = "145ccdc1-6c51-4e45-a04e-21bdea03d170"
