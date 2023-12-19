f80dc0d2f2344e49a11c60aa5c35811523788bf3
shankardhandapani <shankardhandapani1@gmail.com>
2023-12-19 15:04:34 +0530

    commenting flux conf temporarily

--- modules/aks/gitops.tf
+++ modules/aks/gitops.tf
@@ -1,27 +1,27 @@
-resource "azurerm_kubernetes_cluster_extension" "flux_ext" {
-  name           = join("-", [var.cluster_name, "flux", "ext"])
-  cluster_id     = azurerm_kubernetes_cluster.k8s.id
-  extension_type = "microsoft.flux"
-}
+# resource "azurerm_kubernetes_cluster_extension" "flux_ext" {
+#   name           = join("-", [var.cluster_name, "flux", "ext"])
+#   cluster_id     = azurerm_kubernetes_cluster.k8s.id
+#   extension_type = "microsoft.flux"
+# }
 
-resource "azurerm_kubernetes_flux_configuration" "flux_conf" {
-  name       = join("-", [var.cluster_name, "flux", "conf"])
-  cluster_id = azurerm_kubernetes_cluster.k8s.id
-  namespace  = "flux"
+# resource "azurerm_kubernetes_flux_configuration" "flux_conf" {
+#   name       = join("-", [var.cluster_name, "flux", "conf"])
+#   cluster_id = azurerm_kubernetes_cluster.k8s.id
+#   namespace  = "flux"
 
-  git_repository {
-    url                    = var.git_repo_url
-    reference_type         = "branch"
-    reference_value        = "main"
-    ssh_private_key_base64 = var.ssh_private_key_base64
-  }
+#   git_repository {
+#     url                    = var.git_repo_url
+#     reference_type         = "branch"
+#     reference_value        = "main"
+#     ssh_private_key_base64 = var.ssh_private_key_base64
+#   }
 
-  kustomizations {
-    name = "apps"
-    path = "clusters/poc-cluster"
-  }
+#   kustomizations {
+#     name = "apps"
+#     path = "clusters/poc-cluster"
+#   }
 
-  depends_on = [
-    azurerm_kubernetes_cluster_extension.flux_ext
-  ]
-}
+#   depends_on = [
+#     azurerm_kubernetes_cluster_extension.flux_ext
+#   ]
+# }
