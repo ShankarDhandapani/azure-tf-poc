329ff8c98add64dfe06646fddbea9c3ead4a433e
shankardhandapani <shankardhandapani1@gmail.com>
2023-12-20 11:08:53 +0530

    removing flux ext for poc

--- modules/aks/gitops.tf
+++ modules/aks/gitops.tf
@@ -1,8 +1,8 @@
-resource "azurerm_kubernetes_cluster_extension" "flux_ext" {
-  name           = join("-", [var.cluster_name, "flux", "ext"])
-  cluster_id     = azurerm_kubernetes_cluster.k8s.id
-  extension_type = "microsoft.flux"
-}
+# resource "azurerm_kubernetes_cluster_extension" "flux_ext" {
+#   name           = join("-", [var.cluster_name, "flux", "ext"])
+#   cluster_id     = azurerm_kubernetes_cluster.k8s.id
+#   extension_type = "microsoft.flux"
+# }
 
 resource "azurerm_kubernetes_flux_configuration" "flux_conf" {
   name       = join("-", [var.cluster_name, "flux", "conf"])
